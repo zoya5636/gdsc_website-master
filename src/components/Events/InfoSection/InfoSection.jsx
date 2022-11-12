1100059f6beb3206082e6ec6ae8811eb214a0e6e
zoya5636 <zoya@chishtie.com>
2022-11-12 11:05:15 -0500

    Added Career Crawl

--- src/components/Events/InfoSection/InfoSection.jsx
+++ src/components/Events/InfoSection/InfoSection.jsx
@@ -2,8 +2,9 @@ import React, { Component } from 'react'
 import { Grid } from "@material-ui/core"
 //import { googleFormsToJson } from 'react-google-forms-hooks'
 import "./InfoSection.css"
-import stock2 from "../../../assets/events/Fall_Mentorship_Program.jpg"
-import stock3 from "../../../assets/events/Weekly_Leetcode.jpg"
+import event1 from "../../../assets/events/Fall_Mentorship_Program.jpg"
+import event2 from "../../../assets/events/Weekly_Leetcode.jpg"
+import event3 from "../../../assets/events/Career_Crawl.jpg"
 
 const form_width = "840"
 const form_height = "800"
@@ -31,7 +32,7 @@ export class InfoSection extends Component {
                             </p> 
                                 </Grid>
                                 <Grid item md={3} spacing={2}>
-                                <img src={stock3} width="300" height="300" alt="stock" />
+                                <img src={event1} width="300" height="300" alt="stock" />
                                 </Grid>
                             </Grid>
                         </div>
@@ -41,7 +42,7 @@ export class InfoSection extends Component {
                         <div className='infoSectionContent'>
                             <Grid container justify="center" spacing={5}>
                                 <Grid item md={3} spacing={2}>
-                                    <img src={stock2} width="300" height="300" alt="stock" />
+                                    <img src={event2} width="300" height="300" alt="stock" />
                                 </Grid>
                                 <Grid item md={4} spacing={2}>
                                     <h2>Fall Mentorship Program</h2>
@@ -50,6 +51,25 @@ export class InfoSection extends Component {
                                         mix of upper-year students in a related field and industry professionals. Participants have the chance to hear from our
                                         mentors in this mentorship program introductory event!
                                     </p> 
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </Grid>
+
+                    <Grid item md={9} spacing={3} style={{backgroundColor: 'white'}}>
+                        <div className='infoSectionContent'>
+                            <Grid container justify="center" spacing={5}>
+                                <Grid item md={5} spacing={2}>
+                                <h2>Career Crawl</h2>
+                            <p>
+                                The Career Crawl will be held from <b>November 21 to 30</b>. It will consist of multiple info sessions and a networking event geared towards exposing students to a variety
+                                of industries, companies, and positions in tech. This event is a great opportunity for students to broaden their knowledge,
+                                gain connections, and ultimately advance their career opportinuties. Stay tuned to learn more about the companies attending
+                                our event and upcoming important dates
+                            </p> 
+                                </Grid>
+                                <Grid item md={3} spacing={2}>
+                                <img src={event3} width="300" height="300" alt="stock" />
                                 </Grid>
                             </Grid>
                         </div>
